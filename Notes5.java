31e0e860eb05262d8c92675fb1998800a0704dea
samgarthwaite <43002471+samgarthwaite@users.noreply.github.com>
2018-10-29 08:17:53 -0400

    Add files via upload

--- Notes5.java
+++ Notes5.java
@@ -1,4 +1,31 @@
 
 public class Notes5 {
-
+/**
+ * length()
+ * 		String str = "Mr. Ellis";
+ * 		int numLength = str.length();
+ * 		System.out.print(nameLength);
+ * 	charAt(index)
+ * 	does index stuff
+ * 	0123456789
+ * 		String str = "How's it going?";
+ * 		char myChar = str.charAt(7):
+ * 	char is equal to letters in the word - 1
+ * 					Last Index is one less than the length
+ *   substring
+ *   returns a new string which is part of the new string
+ *   	str.substring(0, 11) <- needs a stop and end 
+ *   start is inclusive, stop is exclusive
+ *   	also can do str.substring(11); <-- Goes from first arg to end
+ *   
+ *  indexOf(char);
+ *  can be used with a char argumet or a string argument
+ *  returns the index of the first occurrence
+ *  	String str = "Welcome.com"
+ *  	str.indexOf('e'); <-- this gets '' instead of "" because it is a char not a string
+ *  this will equal one
+ *  	str.indexOf("com");
+ *  will return as 3, because this is where it begins
+ *  
+ */
 }
