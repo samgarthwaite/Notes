85338c7f63c0aeadf65916226dda95081bf8b2ba
samgarthwaite <43002471+samgarthwaite@users.noreply.github.com>
2018-11-02 08:39:20 -0400

    Add files via upload

---
+++ Notes6.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ * @author Sam Garthwaite
+ *
+ */
+public class Notes6 
+{
+	public static void main(String[] args)
+	{
+		/**
+		 * Control Structure babyee
+		 * 
+		 * If 
+		 * Use boolean to determine how to run the next block of the code
+		 * 		if (coldOutside)
+		 * 			wearCoat();
+		 * or
+		 * 		if (coldOutside)
+		 * 		{
+		 * 			wearCoat;
+		 * 			wearHat;
+		 * 			wearGloves();
+		 * 		{
+		 * 
+		 */
+	}
+}
